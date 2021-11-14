40c6b2c61b056566dfcedc1182b0559d38523ba8
Hugo Heden <hugoheden@fastmail.fm>
2021-11-14 17:08:07 +0100

    Add demo examples

--- demo.sql
+++ demo.sql
@@ -1,188 +1,268 @@
-
-create or replace function demo(inout description text,
-                                inout doc jsonb,
+drop function demo;
+create or replace function demo(inout doc jsonb,
                                 inout selection jsonb,
-                                inout expected_result jsonb,
                                 out result jsonb,
-                                out correct boolean)
+                                inout expected_result jsonb,
+                                out correct boolean,
+                                inout comment text)
     language sql
     immutable
     parallel safe
 as
 $$
 select
-    description,
     doc,
     selection,
-    expected_result,
     r.result,
-    coalesce(r.result = expected_result, ((r.result is null) = (expected_result is null)))
+    expected_result,
+    coalesce(r.result = expected_result, ((r.result is null) = (expected_result is null))),
+    comment
 from jsonb_select_keys(doc, selection) r(result)
 $$;
 
 select
-    (
-        demo('', '{"a": {"b": "blabla", "c": "bla"}}',
-             '{}', '{}')
+    (demo('{"a": {"b": "blabla", "c": "bla"}}',
+          '{}',
+          '{}',
+          '')
         ).*
 union all
 select
-    (
-        demo('', '{"a": {"b": "blabla", "c": "bla"}}',
-             '{"a": {"b": 1, "c": 1}}', '{"a": {"b": "blabla", "c": "bla"}}')
+    (demo('{"a": {"b": "blabla", "c": "bla"}}',
+          '{"a": {"b": 1, "c": 1}}',
+          '{"a": {"b": "blabla", "c": "bla"}}',
+          '')
         ).*
 union all
 select
-    (
-        demo('', '{"a": {"b": "blabla", "c": "bla"}}',
-             '{"a": {"b": 1}}', '{"a": {"b": "blabla"}}')
+    (demo('{"a": {"b": "blabla", "c": "bla"}}',
+          '{"a": {"b": 1}}',
+          '{"a": {"b": "blabla"}}',
+          '')
         ).*
 union all
 select
-    (
-        demo('', '{"a": {"b": "blabla", "c": "bla"}}',
-             '{"a": {"d": 1}}', '{"a": {}}')
+    (demo('{"a": {"b": "blabla", "c": "bla"}}',
+          '{"a": {"d": 1}}',
+          '{"a": {}}',
+          '')
         ).*
 union all
 select
-    (
-        demo('Mongo: {"a": {}}', '{"a": {"b": "blabla", "c": "bla"}}',
-             '{"a": {"b": {"D": 1}}}', '{"a": {"b": "blabla"}}')
+    (demo('{"a": {"b": "blabla", "c": "bla"}}',
+          '{"a": {"b": {"D": 1}}}',
+          '{"a": {"b": "blabla"}}',
+          'Mongo: {"a": {}}')
+        ).*
+union all
+select
+    (demo('{"a": {"b": "blabla", "c": "bla"}}',
+          '{"a": {}}',
+          '{"a": {}}',
+          'Mongo: "Error An empty sub-projection is not a valid value"')
+        ).*
+union all
+select
+    (demo('{"a": {"b": {"c": "bla"}}}',
+          '{"a": {"b": {"c": 1}}}',
+          '{"a": {"b": {"c": "bla"}}}',
+          '')
+        ).*
+union all
+select
+    (demo('{"a": {"b": {"c": "bla"}}}',
+          '{"a": {"b": {"c": {"D": 1}}}}',
+          '{"a": {"b": {"c": "bla"}}}',
+          'Mongo: {"a": {"b": {}}}')
+        ).*
+union all
+select
+    (demo('{"a": {"b": {"c": "bla"}}}',
+          '{"a": {"b": {"E": 1}}}',
+          '{"a": {"b": {}}}',
+          '')
+        ).*
+union all
+select
+    (demo('{"a": {"b": {"c": "bla"}}}',
+          '{"a": {"E": {"F": 1}}}',
+          '{"a": {}}',
+          '')
+        ).*
+union all
+select
+    (demo('{"ls": ["x", "y", "z"]}',
+          '{}',
+          '{}',
+          '')
+        ).*
+union all
+select
+    (demo('{"ls": ["x", "y", "z"]}',
+          '{"ls": 1}',
+          '{"ls": ["x", "y", "z"]}',
+          '')
+        ).*
+union all
+select
+    (demo('{"ls": ["x", "y", "z"]}',
+          '{"a": 1}',
+          '{}',
+          '')
+        ).*
+union all
+select
+    (demo('{"ls": ["x", "y", "z"]}',
+          '{"ls": {"a": 1}}',
+          '{"ls": ["x", "y", "z"]}',
+          'Mongo: {"ls": []}') -- Somewhat interesting
+        ).*
+union all
+select
+    (demo('{"ls": [{"a": "x"}, {"a": "y"}, {"b": "z"}]}',
+          '{}',
+          '{}',
+          '')
+        ).*
+union all
+select
+    (demo('{"ls": [{"a": "x"}, {"a": "y"}, {"b": "z"}]}',
+          '{"ls": 1}',
+          '{"ls": [{"a": "x"}, {"a": "y"}, {"b": "z"}]}',
+          '')
+        ).*
+union all
+select
+    (demo('{"ls": [{"a": "x"}, {"a": "y"}, {"b": "z"}]}',
+          '{"a": 1}',
+          '{}',
+          '')
+        ).*
+union all
+select
+    (demo('{"ls": [{"a": "x"}, {"a": "y"}, {"b": "z"}]}',
+          '{"ls": {"a": 1}}',
+          '{"ls": [{"a": "x"}, {"a": "y"}, {}]}',
+          '')
+        ).*
+union all
+select
+    (demo('{"ls": [{"a": "x", "b": "xx"}, {"a": "y", "b": "yy"}, {"a": "z", "b": "zz"}]}',
+          '{"ls": {"a": 1}}',
+          '{"ls": [{"a": "x"}, {"a": "y"}, {"a": "z"}]}',
+          '')
+        ).*
+union all
+select
+    (demo('{"ls": [{"a": "x", "b": "xx"}, {"a": "y", "b": "yy"}, {"a": "z", "b": "zz"}]}',
+          '{"ls": {"c": 1}}',
+          '{"ls": [{}, {}, {}]}',
+          '') -- interesting
+        ).*
+union all
+select
+    (demo('{"ls": [["a", "b"], ["c"], [], {"a": "x", "d": "y"}, {"e": "z"}]}',
+          '{"ls": {}}',
+          '{"ls": [["a", "b"], ["c"], [], {}, {}]}',
+          'Mongo: {"ls": [[], [], [], {}, {}]}') -- TODO - double check Mongo
+        ).*
+union all
+select
+    (demo('{"ls": [["a", "b"], ["c"], [], {"a": "x", "d": "y"}, {"e": "z"}]}',
+          '{"ls": 1}',
+          '{"ls": [["a", "b"], ["c"], [], {"a": "x", "d": "y"}, {"e": "z"}]}',
+          '')
+        ).*
+union all
+select
+    (demo('{"ls": [["a", "b"], ["c"], [], {"a": "x", "d": "y"}, {"e": "x"}]}',
+          '{"ls": {"a": 1, "d": 1}}',
+          '{"ls": [["a", "b"], ["c"], [], {"a": "x", "d": "y"}, {}]}',
+          'Mongo: {"ls": [[], [], [], {"a": "x", "d": "y"}, {}]}')
+        ).*
+union all
+select
+    (demo('{"ls": [["a", "b"], ["c"], [], {"a": "x", "d": "y"}, {"e": "z"}]}',
+          '{"ls": {"a": 1, "d": 1, "e": 1}}',
+          '{"ls": [["a", "b"], ["c"], [], {"a": "x", "d": "y"}, {"e": "z"}]}',
+          'Mongo: {"ls": [[], [], [], {"a": "x", "d": "y"}, {"e": "z"}]}')
+        ).*
+union all
+select
+    (demo('{"ls": [["a", "b"], ["c"], [], {"a": "x", "d": "y"}, {"e": "z"}]}',
+          '{"ls": {"a": 1, "e": 1}}',
+          '{"ls": [["a", "b"], ["c"], [], {"a": "x"}, {"e": "z"}]}',
+          'Mongo: {"ls": [[], [], [], {"a": "x"}, {"e": "z"}]}')
+        ).*
+union all
+select
+    (demo('{}',
+          '{}',
+          '{}',
+          '')
+        ).*
+union all
+select
+    (demo('17',
+          '{}',
+          '17',
+          'This is a bit weird. An object in the selection - even if empty - matches a scalar in the document')
+        ).*
+union all
+select
+    (demo('"abc"',
+          '{}',
+          '"abc"',
+          'This is a bit weird. An object in the selection - even if empty - matches a scalar in the document')
+        ).*
+union all
+select
+    (demo('null',
+          '{}',
+          'null',
+          'This is a bit weird. An object in the selection - even if empty - matches a scalar in the document')
         ).*
 
--- test_stuff('{"a": {"b": "blabla", "c": "bla"}}, 1); // ERROR "MongoServerError: Expected field projectionto be of type object"
--- // test_stuff('{"a": {"b": "blabla", "c": "bla"}}, null); // {"_id": {"$oid": "618d959b416c1a278f47f3eb"}, "a": {"b": "blabla", "c": "bla"}} -- just uninteresting - the selection document is ignored sort of'
--- // test_stuff('{"a": {"b": "blabla", "c": "bla"}}, {"_id": 0, "a": {}}); // Interesting ERROR: Query failed with error code 51270 and error message 'An empty sub-projection is not a valid value. Found empty object at path' on server localhost:27017'
 union all
 select
-    (
-        demo('', '{"a": {"b": {"c": "bla"}}}',
-             '{"a": {"b": {"c": 1}}}', '{"a": {"b": {"c": "bla"}}}')
+    (demo('["x", "y", "z"]',
+          '{}',
+          '["x", "y", "z"]',
+          'This is a bit weird...?')
         ).*
 union all
 select
-    (
-        demo('Mongo: {"a": {"b": {}}}', '{"a": {"b": {"c": "bla"}}}',
-             '{"a": {"b": {"c": {"D": 1}}}}', '{"a": {"b": {"c": "bla"}}}')
+    (demo('["x", "y", "z"]',
+          '{}',
+          '["x", "y", "z"]',
+          'This is a bit weird...?')
         ).*
 union all
 select
-    (
-        demo('', '{"a": {"b": {"c": "bla"}}}',
-             '{"a": {"b": {"E": 1}}}', '{"a": {"b": {}}}')
+    (demo('["x", "y", "z"]',
+          '{"a": 1}',
+          '["x", "y", "z"]',
+          'This is a bit weird...?')
         ).*
 union all
 select
-    (
-        demo('', '{"a": {"b": {"c": "bla"}}}',
-             '{"a": {"E": {"F": 1}}}', '{"a": {}}')
+    (demo('[{"a": "x"}, {"a": "y"}, {"b": "z"}]',
+          '{}',
+          '[{}, {}, {}]',
+          '')
         ).*
 union all
 select
-    (
-        demo('', '{"ls": ["1", "3", "5"]}',
-             '{}', '{}')
+    (demo('[{"a": "x"}, {"a": "y"}, {"b": "z"}]',
+          '{"a": 1}',
+          '[{"a": "x"}, {"a": "y"}, {}]',
+          '')
         ).*
 union all
 select
-    (
-        demo('', '{"ls": ["1", "3", "5"]}',
-             '{"ls": 1}', '{"ls": ["1", "3", "5"]}')
+    (demo('[{"a": "x"}, {"a": "y"}, {"b": "z"}]',
+          '{"a": {"b": 1}}',
+          '[{"a": "x"}, {"a": "y"}, {}]',
+          '')
         ).*
-union all
-select
-    (
-        demo('', '{"ls": ["1", "3", "5"]}',
-             '{"a": 1}', '{}')
-        ).*
-union all
-select
-    (
-        demo('Monge: {"ls": []}', '{"ls": ["1", "3", "5"]}',
-             '{"ls": {"a": 1}}', '{"ls": ["1", "3", "5"]}') -- Somewhat interesting
-        ).*
-union all
-select
-    (
-        demo('', '{"ls": [{"a": "1"}, {"a": "3"}, {"a": "5"}, {"c": "hello"}]}',
-             '{}', '{}')
-        ).*
-union all
-select
-    (
-        demo('', '{"ls": [{"a": "1"}, {"a": "3"}, {"a": "5"}]}',
-             '{"ls": 1}', '{"ls": [{"a": "1"}, {"a": "3"}, {"a": "5"}]}')
-        ).*
-union all
-select
-    (
-        demo('', '{"ls": [{"a": "1"}, {"a": "3"}, {"a": "5"}]}',
-             '{"a": 1}', '{}')
-        ).*
-union all
-select
-    (
-        demo('', '{"ls": [{"a": "1"}, {"a": "3"}, {"a": "5"}]}',
-             '{"ls": {"a": 1}}', '{"ls": [{"a": "1"}, {"a": "3"}, {"a": "5"}]}')
-        ).*
-union all
-select
-    (
-        demo('', '{"ls": [{"a": "1", "b": "2"}, {"a": "3", "b": "4"}, {"a": "5", "b": "6"}]}',
-             '{"ls": {"a": 1}}', '{"ls": [{"a": "1"}, {"a": "3"}, {"a": "5"}]}')
-        ).*
-union all
-select
-    (
-        demo('', '{"ls": [{"a": "1", "b": "2"}, {"a": "3", "b": "4"}, {"a": "5", "b": "6"}]}',
-             '{"ls": {"CDE": 1}}', '{"ls": [{}, {}, {}]}') -- interesting
-        ).*
-union all
-select
-    (
-        demo('Mongo: {"ls": [[], [], [], {}, {}]}',
-             '{"ls": [["a", "b"], ["c"], [], {"a": "bla", "d": "bluh"}, {"e": "bleh"}]}',
-             '{"ls": {}}', '{"ls": [["a", "b"], ["c"], [], {}, {}]}') -- TODO - double check Mongo
-        ).*
-union all
-select
-    (
-        demo('', '{"ls": [["a", "b"], ["c"], [], {"a": "bla", "d": "bluh"}, {"e": "bleh"}]}',
-             '{"ls": 1}', '{"ls": [["a", "b"], ["c"], [], {"a": "bla", "d": "bluh"}, {"e": "bleh"}]}')
-        ).*
-union all
-select
-    (
-        demo('Mongo: {"ls": [[], [], [], {"a": "bla", "d": "bluh"}, {}]}',
-             '{"ls": [["a", "b"], ["c"], [], {"a": "bla", "d": "bluh"}, {"e": "bleh"}]}',
-             '{"ls": {"a": 1, "d": 1}}', '{"ls": [["a", "b"], ["c"], [], {"a": "bla", "d": "bluh"}, {}]}')
-        ).*
-union all
-select
-    (
-        demo('Mongo: {"ls": [[], [], [], {"a": "bla", "d": "bluh"}, {"e": "bleh"}]}',
-             '{"ls": [["a", "b"], ["c"], [], {"a": "bla", "d": "bluh"}, {"e": "bleh"}]}',
-             '{"ls": {"a": 1, "d": 1, "e": 1}}',
-             '{"ls": [["a", "b"], ["c"], [], {"a": "bla", "d": "bluh"}, {"e": "bleh"}]}')
-        ).*
-union all
-select
-    (
-        demo('Mongo: {"ls": [[], [], [], {"a": "bla"}, {"e": "bleh"}]}',
-             '{"ls": [["a", "b"], ["c"], [], {"a": "bla", "d": "bluh"}, {"e": "bleh"}]}',
-             '{"ls": {"a": 1, "e": 1}}', '{"ls": [["a", "b"], ["c"], [], {"a": "bla"}, {"e": "bleh"}]}')
-        ).*;
-
-
--- union all select (
--- test_stuff({}, {"_id": 0}); // {}
--- test_stuff({}, {"_id": 1}); // {"_id": {"$oid": "618e8f983ab9185a982c160f"}}
--- test_stuff(17, {"_id": 0}); // {}
--- test_stuff(17, {"_id": 1}); // {"_id": {"$oid": "618e8fa13ab9185a982c1611"}}
--- test_stuff("abc", {"_id": 0}); // WEIRD? {"0": "a", "1": "b", "2": "c"}
--- test_stuff(null, {"_id": 0}); // {}
--- test_stuff(["1", "3", "5"], {"_id": 0}); // WEIRD {"0": "1", "1": "3", "2": "5"}
--- test_stuff(["1", "3", "5"], {"_id": 0, "a": 1}); // {}
--- test_stuff([{"a": "1"}, {"a": "3"}, {"a": "5"}], {"_id": 0}); // WEIRD {"0": {"a": "1"}, "1": {"a": "3"}, "2": {"a": "5"}}
--- test_stuff([{"a": "1"}, {"a": "3"}, {"a": "5"}], {"_id": 0, "a": 1}); // {}
--- test_stuff([{"a": "1"}, {"a": "3"}, {"a": "5"}], {"_id": 0, "a": {"b": 1}}); // {}
+;
