f9bf670e29040486b5d74d672ed0935c4f76b301
Hugo Heden <hugoheden@fastmail.fm>
2022-06-29 21:57:10 +0200

    Hmm, let IntelliJ auto-indent test code

--- test/test.sql
+++ test/test.sql
@@ -21,154 +21,155 @@ from jsonb_select_keys(doc, selection) r(result)
 $$;
 
 drop table if exists example_doc;
-create table example_doc(x) as (
-    select '
-           {
-             "a": "ax",
-             "a2": "a2x",
-             "b_obj": {
-               "ba": "bax",
-               "bb": "bbx"
-             },
-             "c_obj": {
-               "ca": "cax",
-               "cb": [
-                 "cbx",
-                 "cby"
-               ],
-               "cc": [
-                 {
-                   "cca": "cca1",
-                   "ccb": "ccb1"
-                 },
-                 {
-                   "cca": "cca2"
-                 },
-                 {
-                   "ccb": "ccb2"
-                 }
-               ]
-             },
-             "d_list": [
-               "dx",
-               "dy"
-             ],
-             "e_list": [
-               {
-                 "ea": "ea1",
-                 "eb": "eb1"
-               },
-               {
-                 "ea": "ea2"
-               },
-               {
-                 "eb": "eb2"
-               }
-             ]
-           }
-           '::jsonb);
+create table example_doc(x)
+as
+    (select '
+            {
+              "a": "ax",
+              "a2": "a2x",
+              "b_obj": {
+                "ba": "bax",
+                "bb": "bbx"
+              },
+              "c_obj": {
+                "ca": "cax",
+                "cb": [
+                  "cbx",
+                  "cby"
+                ],
+                "cc": [
+                  {
+                    "cca": "cca1",
+                    "ccb": "ccb1"
+                  },
+                  {
+                    "cca": "cca2"
+                  },
+                  {
+                    "ccb": "ccb2"
+                  }
+                ]
+              },
+              "d_list": [
+                "dx",
+                "dy"
+              ],
+              "e_list": [
+                {
+                  "ea": "ea1",
+                  "eb": "eb1"
+                },
+                {
+                  "ea": "ea2"
+                },
+                {
+                  "eb": "eb2"
+                }
+              ]
+            }
+            '::jsonb);
 
 drop table if exists test_results_example_doc;
 create table test_results_example_doc as
-with example_selections(selection, expected_result, comment) as (
-    values (
-                -- selection:
-                '{
-                  "a": 1
-                 }'::jsonb,
-                -- expected result:
-                '{
-                  "a": "ax"
-                 }'::jsonb,
-                'Select a property at the root level'),
+with example_selections(selection, expected_result, comment)
+         as
+         (values (
+                     -- selection:
+                     '{
+                       "a": 1
+                     }'::jsonb,
+                     -- expected result:
+                     '{
+                       "a": "ax"
+                     }'::jsonb,
+                     'Select a property at the root level'),
 
-           (
-               -- selection:
-                '{
-                    "a": 1,
-                    "b_obj": {
-                      "bb": 1
-                    }
-                 }'::jsonb,
-                -- expected result:
-                '{
-                    "a": "ax",
-                    "b_obj": {
-                      "bb": "bbx"
-                    }
-                 }'::jsonb,
-                'Select a property at the root level, and one property in a sub-object'),
+                 (
+                     -- selection:
+                     '{
+                       "a": 1,
+                       "b_obj": {
+                         "bb": 1
+                       }
+                     }'::jsonb,
+                     -- expected result:
+                     '{
+                       "a": "ax",
+                       "b_obj": {
+                         "bb": "bbx"
+                       }
+                     }'::jsonb,
+                     'Select a property at the root level, and one property in a sub-object'),
 
-           (
-               -- selection:
-                '{
-                    "a": 1,
-                    "d_list": 1
-                  }'::jsonb,
-               -- expected result:
-                '{
-                    "a": "ax",
-                    "d_list": [
-                      "dx",
-                      "dy"
-                    ]
-                  }'::jsonb,
-                'Select a property and a list at the root level'),
+                 (
+                     -- selection:
+                     '{
+                       "a": 1,
+                       "d_list": 1
+                     }'::jsonb,
+                     -- expected result:
+                     '{
+                       "a": "ax",
+                       "d_list": [
+                         "dx",
+                         "dy"
+                       ]
+                     }'::jsonb,
+                     'Select a property and a list at the root level'),
 
-           (
-               -- selection:
-               '{
-                  "a": 1,
-                  "e_list": {
-                    "ea": 1
-                  }
-                }'::jsonb,
-               -- expected result:
-               '{
-                 "a": "ax",
-                 "e_list": [
-                   {
-                     "ea": "ea1"
-                   },
-                   {
-                     "ea": "ea2"
-                   },
-                   {}
-                 ]
-               }'::jsonb,
-               'Select a property and parts of sub-objects within a list'),
+                 (
+                     -- selection:
+                     '{
+                       "a": 1,
+                       "e_list": {
+                         "ea": 1
+                       }
+                     }'::jsonb,
+                     -- expected result:
+                     '{
+                       "a": "ax",
+                       "e_list": [
+                         {
+                           "ea": "ea1"
+                         },
+                         {
+                           "ea": "ea2"
+                         },
+                         {}
+                       ]
+                     }'::jsonb,
+                     'Select a property and parts of sub-objects within a list'),
 
-           (
-               -- selection:
-               '{
-                 "c_obj": {
-                   "cc": {
-                     "ccb": 1
-                   }
-                 },
-                 "b_obj": {
-                   "bb": 1
-                 }
-               }'::jsonb,
-               -- expected result:
-               '{
-                 "b_obj": {
-                   "bb": "bbx"
-                 },
-                 "c_obj": {
-                   "cc": [
-                     {
-                       "ccb": "ccb1"
-                     },
-                     {},
-                     {
-                       "ccb": "ccb2"
-                     }
-                   ]
-                 }
-               }'::jsonb,
-               'Select a property and parts of sub-objects within a list within a sub-object, and a property in a sub-object ')
-    )
+                 (
+                     -- selection:
+                     '{
+                       "c_obj": {
+                         "cc": {
+                           "ccb": 1
+                         }
+                       },
+                       "b_obj": {
+                         "bb": 1
+                       }
+                     }'::jsonb,
+                     -- expected result:
+                     '{
+                       "b_obj": {
+                         "bb": "bbx"
+                       },
+                       "c_obj": {
+                         "cc": [
+                           {
+                             "ccb": "ccb1"
+                           },
+                           {},
+                           {
+                             "ccb": "ccb2"
+                           }
+                         ]
+                       }
+                     }'::jsonb,
+                     'Select a property and parts of sub-objects within a list within a sub-object, and a property in a sub-object '))
 select (run_test_case(d.x, e.selection, e.expected_result, e.comment)).*
 from example_doc d
          cross join example_selections e;
@@ -179,13 +180,13 @@ drop table if exists test_results_many_small;
 create table test_results_many_small as
 select (run_test_case(t.doc::jsonb, t.selection::jsonb, t.expected_result::jsonb, t.comment)).*
 from (values ('{"a": "blabla", "b": {"c": "bleh", "d":  "bluh"}}', -- doc
-              '{"a": 1}',                                          -- selection
-              '{"a": "blabla"}',                                   -- expected result
-              '')                                                  -- comment
+              '{"a": 1}', -- selection
+              '{"a": "blabla"}', -- expected result
+              '')                  -- comment
            , ('{"a": "blabla", "b": {"c": "bleh", "d":  "bluh"}}', -- doc
-              '{"b": 1}',                                          -- selection
-              '{"b": {"c": "bleh", "d":  "bluh"}}',                -- expected result
-              '')                                                  -- comment
+              '{"b": 1}', -- selection
+              '{"b": {"c": "bleh", "d":  "bluh"}}', -- expected result
+              '')                  -- comment
            , ('{"a": "blabla", "b": {"c": "bleh", "d":  "bluh"}}',
               '{"b": {"c":  1}}',
               '{"b": {"c": "bleh"}}',
@@ -341,6 +342,5 @@ from (values ('{"a": "blabla", "b": {"c": "bleh", "d":  "bluh"}}', -- doc
            , ('[{"a": "x"}, {"a": "y"}, {"b": "z"}]',
               '{"a": {"b": 1}}',
               '[{"a": "x"}, {"a": "y"}, {}]',
-              '')
-     ) t("doc", selection, expected_result, comment);
+              '')) t("doc", selection, expected_result, comment);
 
